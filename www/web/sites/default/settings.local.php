bc1db50be59dd233fab1261b4fe2ecb65793c4e1
Rafael Franco de Oliveira <rafaelo@appnovation.com>
2020-08-13 23:50:46 -0300

    Including the default settings.* files for local setup.

---
+++ www/web/sites/default/settings.local.php
@@ -0,0 +1,17 @@
+<?php
+
+if (getenv('LANDO') === 'ON') {
+  $settings['trusted_host_patterns'] = ['.*'];
+  $settings['hash_salt'] = md5(getenv('LANDO_HOST_IP'));
+  $settings["config_sync_directory"] = 'config/sync';
+
+  $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
+  $databases['default']['default'] = [
+    'driver' => 'mysql',
+    'database' => $lando_info['database']['creds']['database'],
+    'username' => $lando_info['database']['creds']['user'],
+    'password' => $lando_info['database']['creds']['password'],
+    'host' => $lando_info['database']['internal_connection']['host'],
+    'port' => $lando_info['database']['internal_connection']['port'],
+  ];
+}
